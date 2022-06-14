f2a8c78ec83e963d9e787ec9bccb0fff605cc713
Aaron Harris <aaronmitchellharris@gmail.com>
2022-06-14 14:46:30 -0700

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocket_url: 'ws://watch-yt-together.herokuapp.com/:8080',
-  server_url: 'https://watch-yt-together.herokuapp.com/:8081'
+  websocket_url: 'ws://watchtogether.onrender.com/:8080',
+  server_url: 'https://watchtogether.onrender.com/:8081'
 };
