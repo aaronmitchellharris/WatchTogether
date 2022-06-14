71dc119141fc126de9f57fc5849aa995ebd3023e
Aaron Harris <aaronmitchellharris@gmail.com>
2022-06-14 13:18:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/lobbies', (req, res) => {
 
 // use angular for frontend
 app.get('/', (req, res) => {
-    res.sendFile('/frontend/src/index');
+    res.sendFile('/frontend/dist/frontend/index');
 });
 
 // start server
