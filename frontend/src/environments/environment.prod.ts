776d926084f1f466e8b2d6b1c951676afcb01ef4
Aaron Harris <aaronmitchellharris@gmail.com>
2022-06-14 19:31:43 -0700

    Deploy updates

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocket_url: 'wss://watchtogether.onrender.com:8080',
+  websocket_url: 'wss://watchtogether.onrender.com/:8008',
   server_url: 'https://watchtogether.onrender.com'
 };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 8081;
-const ws_PORT = process.env.ws_PORT || 8080;
+const PORT = process.env.PORT || 8012;
+const ws_PORT = process.env.ws_PORT || 8008;
 
 // create website server
 const express = require('express');
