a3a9a02c01efb81c5e0a9283684ff68a2c854bee
Aaron Harris <aaronmitchellharris@gmail.com>
2022-06-14 17:41:05 -0700

    Deploy updates

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocket_url: 'wss://watchtogether.onrender.com/:31313',
+  websocket_url: 'wss://watchtogether.onrender.com:31313',
   server_url: 'https://watchtogether.onrender.com'
 };
